9fe1fbb274f2402339cd202b05e676a8660a662a
firewoody237 <firewoody237@gmail.com>
2024-05-15 09:48:22 +0900

    thymeleaf + Spring 학습

--- src/main/java/firewoody/learnspring/thymeleaf/ThymeleafController.java
+++ src/main/java/firewoody/learnspring/thymeleaf/ThymeleafController.java
@@ -1,19 +1,22 @@
 package firewoody.learnspring.thymeleaf;
 
+import firewoody.learnspring.thymeleaf.domain.DeliveryCode;
+import firewoody.learnspring.thymeleaf.domain.Item;
+import firewoody.learnspring.thymeleaf.domain.ItemType;
 import firewoody.learnspring.thymeleaf.domain.User;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+@Slf4j
 @Controller
 @RequestMapping("/thymeleaf")
 public class ThymeleafController {
@@ -137,16 +140,53 @@ public class ThymeleafController {
 
     @GetMapping("/template/fragment")
     public String template() {
-        return "template/fragment/fragmentMain";
+        return "thymeleaf/template/fragment/fragmentMain";
     }
 
     @GetMapping("/template/layout")
     public String layout() {
-        return "template/layout/layoutMain";
+        return "thymeleaf/template/layout/layoutMain";
     }
 
     @GetMapping("/template/layoutExtend")
     public String layoutExtends() {
-        return "template/layoutExtend/layoutExtendMain";
+        return "thymeleaf/template/layoutExtend/layoutExtendMain";
+    }
+
+    @GetMapping("/input-form")
+    public String inputForm(Model model) {
+        // 체크 박스
+        // 멀티 체크 박스
+        // 라디오 버튼
+        // 셀렉트
+        model.addAttribute("item", new Item());
+        return "thymeleaf/inputForm";
+    }
+
+    // `@ModelAttribute`를 사용하면, Controller의 Model에 자동으로 해당 요소를 추가한다.
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        // 체크 박스 용도
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        // 라디오 버튼 용도
+        return ItemType.values();
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        // 셀렉트 용도
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
     }
 }
---
+++ src/main/java/firewoody/learnspring/thymeleaf/domain/Item.java
@@ -0,0 +1,27 @@
+package firewoody.learnspring.thymeleaf.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Item {
+
+    private long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    private Boolean open;
+    private List<String> regions;
+    private ItemType itemType;
+    private String deliveryCode;
+
+    public Item() {}
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
