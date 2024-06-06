5496c95734a2aa069a8c0945f2045b7d54aef097
firewoody237 <firewoody237@gmail.com>
2024-06-06 10:10:14 +0900

    Spring - AOP 실전 예시

---
+++ src/test/java/firewoody/learnspring/advanced/app/aoptest/ExamService.java
@@ -0,0 +1,16 @@
+package firewoody.learnspring.advanced.app.aoptest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+}
